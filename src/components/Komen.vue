2329db00a9bd7b5bec2d6e5f91cb1cd94e251bc7
mrosyidridho06 <rridho335@gmail.com>
2020-06-06 01:43:45 +0800

    update

---
+++ src/components/Komen.vue
@@ -0,0 +1,29 @@
+<template>
+ <div>
+    <form v-on:submit.prevent="addNewTodo">
+        <label for="new-todo">Komen</label>
+        <textarea class="textarea" placeholder="e.g. Hello world"></textarea>
+        <button>Add</button>
+    </form>
+    <ul>
+        <li
+        is="todo-item"
+        v-for="(todo, index) in todos"
+        v-bind:key="todo.id"
+        v-bind:title="todo.title"
+        v-on:remove="todos.splice(index, 1)"
+        ></li>
+    </ul>
+ </div>
+</template>
+
+<script>
+export default {
+  name: 'Komentar',
+  data () {
+    return {
+      msg: 'HHH'
+    }
+  }
+}
+</script>
