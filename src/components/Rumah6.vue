ce55d6ae1092554168f65170773fb0d843848cba
Bona Matanari <61195048+bona-098@users.noreply.github.com>
2020-06-06 08:35:26 +0800

    Update Rumah6.vue

--- src/components/Rumah6.vue
+++ src/components/Rumah6.vue
@@ -44,6 +44,7 @@
                         </span>
                         Jl. Banggeris Kec. Sungai Kunjang, Kota Samarinda, Kalimantan Timur
                     </p>
+                    </br>
                 </section>
                 <section class="kolom-data">
                     <h3 class="title is-5">Details</h3>
@@ -62,6 +63,7 @@
                     <p>
                         Lahan(m2): <b>60</b>
                     </p>
+                    </br>
                 </section>
                 <section class="kolom-data">
                     <h3 class="title is-5">Fasilitas</h3>
@@ -74,6 +76,7 @@
                     <p>
                         Taman Bermain
                     </p>
+                    </br>
                 </section>
                 <section class="kolom-data">
                     <h3 class="title is-5">Lokasi</h3>
